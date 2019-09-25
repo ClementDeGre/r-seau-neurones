4077df1aa7eb3507464ea4e64e23e7c0c203328d
Clement <nguyen.clement91@gmail.com>
2019-09-25 09:47:27 +0200

    getters_setters

--- reseau.py
+++ reseau.py
@@ -14,4 +14,37 @@ class Reseau:
 		self.values=[] #le tableau avec valeurs des neurones
 
 		self.control=0 #controleur pour empecher ajout de couches/neurones après premier lancement
-print('ok')
+	
+	def set_name(self,name):
+		self.name=name
+
+	def get_name(self):
+		return self.name
+
+	def set_erreur(self,nbr):
+		if nbr>0:
+			self.error=nbr
+
+	def get_erreur(self):
+		return self.error
+
+	def set_fun_learn(self,name):
+		if str.lower(name)=='tangente':
+			self.fun_learn=tangente
+			self.name_fun_learn='tangente'
+		else:
+			self.fun_learn=sigmoide
+			self.name_fun_learn='sigmoide'
+
+	def get_name_fun_learn(self):
+		return self.get_name_fun_learn
+
+	def get_data(self):
+		return(self.get_name(),self.get_name_fun_learn(),self.get_erreur(),self.get_nbr_couche())
+
+	def get_nbr_couches(self):
+		return len(self.couches)
+
+	def get_last_couche(self):
+		return self.values[-1]
+
