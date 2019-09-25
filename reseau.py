5659461e0e31e5f22dce63067ff8f1e59fcc8c40
Clement <nguyen.clement91@gmail.com>
2019-09-25 10:43:16 +0200

    génération couches

--- reseau.py
+++ reseau.py
@@ -50,3 +50,43 @@ class Reseau:
 	def get_last_couche(self):
 		return self.values[-1]
 
+	def set_couche(self,value=2):
+		#2 couches au min : in et out
+
+		if self.control==0:
+			if value>=2:
+				for i in range(0,value):
+					self.couche.append(0)
+			else:
+				print("il faut au moins 2 couches")
+		else:
+			print("le réseau est déjà créé, pas de modifs possibles")
+
+	def add_couche(self,pos):
+		if (self.control==0):
+			if (pos>=0 and pos<len(self.couche)):
+				self.couche.insert(pos,0)
+			else:
+				print("vous pouvez insérer une couche dans l'intervalle 0 - {}".format(len(self.couche)))
+		else:
+			print("le réseau est déjà créé, pas de modifs possibles")
+
+	def add_neurone(self,couche,nbr=1):
+		#ajouter au min 1 neurone sur la couche voulue
+		if self.control==0:
+			if (couche>=0 and couche<=len(self.couche)-1 and nbr>0):
+				self.couche[couche]+=nbr
+		else :
+			print("le réseau est déjà créé, pas de modifs possibles")
+
+	def add_all_neurones(self,tab):
+		#si on veut ajouter tous les neurones
+		#ex : si on veut ajouter 5 neurones à la couche 1 et 3 à la couche 2, la première val de tab sera 5 et la 2e 3
+		if self.control==0:
+			if len(tab)==len(self.couche):
+				for i in range(0,len(tab)):
+					self.add_neurone(i,tab[i])
+			else :
+				print("le tableau doit être de taille",len(self.couche))
+		else:
+			print("le réseau est déjà créé, pas de modifs possibles")
